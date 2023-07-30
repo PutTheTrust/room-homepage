f4e141942c343634423b0998bb54d35f3220eb62
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-07-30 10:50:51 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-xl text-blue-700">Room Home page</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-gray": "hsl(0, 0%, 63%)",
+        "very-dark-gray": "hsl(0, 0%, 27%)",
+      },
+    },
+  },
+  plugins: [],
+};
